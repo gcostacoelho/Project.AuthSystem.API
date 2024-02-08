b6cf09b20ff0cf566922c8c22e0f672b16d2039b
Gustavo <gcostacoelho2003@gmail.com>
2024-02-08 13:21:17 -0300

    feat: created a facade to integrate userService with smtpService

--- src/Models/Utils/Email.cs
+++ src/Models/Utils/Email.cs
@@ -1,8 +1,8 @@
 namespace Project.AuthSystem.API.src.Models.Utils;
-public class Email
+public class Email(string fullname, string email, string subject, string body)
 {
-    public string Name { get; set; }
-    public string To { get; set; }
-    public string Subject { get; set; }
-    public string Body { get; set; }
+    public string Name { get; set; } = fullname;
+    public string To { get; set; } = email;
+    public string Subject { get; set; } = subject;
+    public string Body { get; set; } = body;
 }
